fddafb0c3e15ddf1a0fb9bab6741fd625ba526b3
Toooo123 <34826963+Toooo123@users.noreply.github.com>
2020-05-16 15:51:17 -0700

    comments

--- predictions.js
+++ predictions.js
@@ -1,17 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // When the event DOMContentLoaded occurs, it is safe to access the DOM
   
-    // When the user scrolls the page, execute myFunction 
     window.addEventListener('scroll', navsticky);
   
-    // Get the navbar
     var navbar = document.getElementById("navbar");
   
-    // Get the offset position of the navbar
     var sticky = navbar.offsetTop;
-  
-    // Add the sticky class to the navbar when you reach its scroll position. 
-    // Remove "sticky" when you leave the scroll position
   
     function navsticky() {
       if (window.pageYOffset >= sticky) {
@@ -22,11 +15,5 @@ document.addEventListener('DOMContentLoaded', function() {
         navbar.classList.remove("sticky");
       }
     }
-  
-    /*Toggle between adding and removing the "responsive" class to topnav
-    when the user clicks on the icon*/
-  
-    function myFunctionForResponsive() {
-      navbar.classList.toggle('responsive');
     }
   })
